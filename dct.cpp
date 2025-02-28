a121fd0f430eee5b8219ed1bc35e7fae9ff68303
Chuks8888 <hugotarczynski@gmail.com>
2025-02-28 11:44:42 +0100

    Restructuring the files of the currently made encoding steps

--- dct.cpp
+++ dct.cpp
@@ -21,6 +21,7 @@ dct::dct()
 {
     if(!init)
     {
+        // Calculating the frequency matrix
         float* ptr = cosines;
         for(int x = 0; x < 8; x++)
             for(int y = 0; y < 8; y++)
